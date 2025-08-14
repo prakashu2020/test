dbd05277f3220a9c9a6862cda51a1dcbf3d0d442
prakashu2020 <learntech.fun@gmail.com>
2025-08-14 10:23:12 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -2,6 +2,8 @@ hungry = input("are you hungry?")
 
 if hungry=="yes":
     print("eat samosa")
-    print("east pizza")
+    print("eat pizza")
+    print("eat burger")
+    print("eat fries")
 else:
     print("water")
